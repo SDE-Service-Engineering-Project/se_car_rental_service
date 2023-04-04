62d2d988cef8e062b5600a06df63d6a3eb6be721
Sahil <sahil.singh98@hotmail.com>
2023-04-04 19:58:11 +0200

    Add Test Cases for Currency Converter Service and Local Date Utils

---
+++ src/test/java/at/ac/fhcampuswien/car_rental/service/currency_converter/CurrencyConverterServiceImplTest.java
@@ -0,0 +1,114 @@
+package at.ac.fhcampuswien.car_rental.service.currency_converter;
+
+import at.ac.fhcampuswien.car_rental.dao.car.CarEntity;
+import at.ac.fhcampuswien.car_rental.dto.currency.ConvertCarPriceDTO;
+import at.ac.fhcampuswien.car_rental.dto.currency.ConvertResultDTO;
+import at.ac.fhcampuswien.car_rental.dto.currency.CurrencyDTO;
+import at.ac.fhcampuswien.car_rental.repository.car.CarRepository;
+import at.ac.fhcampuswien.car_rental.soap.client.CurrencyConversionService;
+import at.ac.fhcampuswien.car_rental.soap.client.StringArray;
+import at.ac.fhcampuswien.car_rental.utils.Utils;
+import com.sun.xml.ws.fault.ServerSOAPFaultException;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CurrencyConverterServiceImplTest {
+    @Mock
+    CurrencyConversionService currencyConversionService;
+    @Mock
+    CarRepository carRepository;
+    @InjectMocks
+    CurrencyConverterServiceImpl currencyConverterService;
+
+    @Test
+    public void should_convert_currency() {
+        Float amount = 200.0f;
+        String fromCurrency = "USD";
+        String toCurrenty = "EUR";
+        Float resultAmount = 210.0f;
+
+        Mockito.when(currencyConversionService.convert(amount, fromCurrency, toCurrenty))
+                .thenReturn(resultAmount);
+
+        ConvertResultDTO result = currencyConverterService.convert(amount, fromCurrency, toCurrenty);
+
+        Assertions.assertEquals(resultAmount, result.amount());
+        Assertions.assertEquals(toCurrenty, result.currency());
+
+    }
+
+    @Test
+    public void should_throw_error_if_something_wrong() {
+        Float amount = 200.0f;
+        String fromCurrency = "USD";
+        String toCurrenty = "EU";
+
+        Mockito.when(currencyConversionService.convert(amount, fromCurrency, toCurrenty))
+                .thenThrow(ServerSOAPFaultException.class);
+
+        Assertions.assertThrows(
+                ResponseStatusException.class, () -> {
+                    currencyConverterService.convert(amount, fromCurrency, toCurrenty);
+                }
+        );
+    }
+
+    @Test
+    public void should_get_all_currencies() {
+        List<String> listOfCurrencies = List.of("EUR", "USD", "ZAR");
+
+        StringArray array = Mockito.mock(StringArray.class);
+        Mockito.when(currencyConversionService.listCurrencies())
+                .thenReturn(array);
+        Mockito.when(array.getString()).thenReturn(listOfCurrencies);
+
+        CurrencyDTO result = currencyConverterService.getAllCurrencies();
+
+        Assertions.assertEquals(listOfCurrencies, result.currencies());
+    }
+
+    @Test
+    public void should_convert_car_price() {
+        CarEntity carEntity = Utils.carEntity();
+        ConvertCarPriceDTO convertCarPriceDTO = new ConvertCarPriceDTO(carEntity.getCarId(), "EUR");
+        float expectedResult = carEntity.getPrice() + 10.0f;
+
+        Mockito.when(carRepository.findById(carEntity.getCarId()))
+                .thenReturn(Optional.of(carEntity));
+        Mockito.when(currencyConversionService.convert(carEntity.getPrice(), carEntity.getCurrency(), convertCarPriceDTO.toCurrency()))
+                .thenReturn(expectedResult);
+
+        ConvertResultDTO result = currencyConverterService.convertCarPrice(convertCarPriceDTO);
+
+        Assertions.assertEquals(expectedResult, result.amount());
+        Assertions.assertEquals(convertCarPriceDTO.toCurrency(), result.currency());
+    }
+
+    @Test
+    public void should_not_convert_non_existing_car() {
+        ConvertCarPriceDTO convertCarPriceDTO = new ConvertCarPriceDTO(2L, "EUR");
+
+        Mockito.when(carRepository.findById(convertCarPriceDTO.carId()))
+                .thenReturn(Optional.empty());
+
+
+        Assertions.assertThrows(
+                ResponseStatusException.class, () -> {
+                    currencyConverterService.convertCarPrice(convertCarPriceDTO);
+                }
+        );
+    }
+}
---
+++ src/test/java/at/ac/fhcampuswien/car_rental/utils/LocalDateUtilsTest.java
@@ -0,0 +1,82 @@
+package at.ac.fhcampuswien.car_rental.utils;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.junit.Assert.fail;
+
+@ExtendWith(MockitoExtension.class)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LocalDateUtilsTest {
+
+    @Test
+    public void should_return_true_because_overlapping() {
+        LocalDateTime startA = LocalDateTime.now().minusDays(5);
+        LocalDateTime endA = LocalDateTime.now().minusDays(2);
+        LocalDateTime startB = LocalDateTime.now().minusDays(2);
+        LocalDateTime endB = LocalDateTime.now().minusDays(3);
+
+        boolean result = LocalDateUtils.isOverlapping(startA, endA, startB, endB);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void should_return_false_because_not_overlapping() {
+        LocalDateTime startA = LocalDateTime.now().minusDays(5);
+        LocalDateTime endA = LocalDateTime.now().minusDays(3);
+        LocalDateTime startB = LocalDateTime.now().minusDays(2);
+        LocalDateTime endB = LocalDateTime.now().minusDays(1);
+
+        boolean result = LocalDateUtils.isOverlapping(startA, endA, startB, endB);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void should_convert_long_to_datetime() {
+        Long toConvert = 1680559200000L;
+        LocalDateTime expectedLocalDateTime = LocalDateTime.of(
+                LocalDate.of(2023, 4, 4),
+                LocalTime.of(0, 0, 0, 0)
+        );
+
+        LocalDateTime result = LocalDateUtils.convertLongToLocalDateTime(toConvert);
+
+        Assertions.assertEquals(expectedLocalDateTime, result);
+    }
+
+    @Test
+    public void should_not_throw_error_to_valid_timestamp() {
+        LocalDateTime start = LocalDateTime.now().minusDays(5);
+        LocalDateTime end = LocalDateTime.now().minusDays(3);
+
+        try {
+            LocalDateUtils.validateTimespan(start, end);
+        } catch(Exception e) {
+            fail("Should not have thrown any exception");
+        }
+    }
+
+    @Test
+    public void should_throw_error_to_invalid_timestamp() {
+        LocalDateTime start = LocalDateTime.now().minusDays(5);
+        LocalDateTime end = LocalDateTime.now().minusDays(6);
+
+
+        Assertions.assertThrows(
+                ResponseStatusException.class, () -> {
+                    LocalDateUtils.validateTimespan(start, end);
+                }
+        );
+    }
+}
